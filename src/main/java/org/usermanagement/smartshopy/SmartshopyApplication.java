d01c561c041696f05dcfeb7cd9e5cf4a6e60bf7d
Dilaly <ismaildilali@gmail.com>
2025-11-25 16:56:12 +0100

    add dependencies for MapStruct - SpringDoc - Configuration MapStruct

---
+++ src/main/java/org/usermanagement/smartshopy/SmartshopyApplication.java
@@ -0,0 +1,13 @@
+package org.usermanagement.smartshopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartshopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/usermanagement/smartshopy/SmartshopyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.usermanagement.smartshopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartshopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
