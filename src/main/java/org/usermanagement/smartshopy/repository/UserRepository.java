f45bd6b58330a1e65a1d57253b57d1a326881d63
Dilaly <ismaildilali@gmail.com>
2025-11-26 11:46:18 +0100

    creating repository User

---
+++ src/main/java/org/usermanagement/smartshopy/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.usermanagement.smartshopy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.usermanagement.smartshopy.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+     Optional<User> findByUsername(String username);
+     boolean existsbyUsername(String username);
+}
