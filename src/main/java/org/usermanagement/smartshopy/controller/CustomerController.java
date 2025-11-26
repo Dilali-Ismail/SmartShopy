31c725d4c46a007ac6551cbb4cbf1f43f04d35e3
Dilaly <ismaildilali@gmail.com>
2025-11-26 20:50:52 +0100

    controller de customer

---
+++ src/main/java/org/usermanagement/smartshopy/controller/CustomerController.java
@@ -0,0 +1,83 @@
+package org.usermanagement.smartshopy.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.usermanagement.smartshopy.dto.request.CreateCustomerDTO;
+import org.usermanagement.smartshopy.dto.request.UpdateCustomerDTO;
+import org.usermanagement.smartshopy.dto.response.CustomerDto;
+import org.usermanagement.smartshopy.enums.CustomerTier;
+import org.usermanagement.smartshopy.service.Customer.CustomerService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> createCustomer(
+            @Valid @RequestBody CreateCustomerDTO dto) {
+
+        CustomerDto customer = customerService.createCustomeer(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
+        List<CustomerDto> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDto> getCustomerById(
+            @Parameter(description = "ID du client", example = "1")
+            @PathVariable Long id) {
+
+        CustomerDto customer = customerService.getCustomer(id);
+        return ResponseEntity.ok(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerDto> updateCustomer(
+            @Parameter(description = "ID du client", example = "1")
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateCustomerDTO dto) {
+
+        CustomerDto customer = customerService. updateCustomer(id, dto);
+        return ResponseEntity.ok(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteCustomer(
+            @Parameter(description = "ID du client", example = "1")
+            @PathVariable Long id) {
+
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok(Map.of("message", "Client supprimé avec succès"));
+    }
+
+    @GetMapping("/tier/{tier}")
+    public ResponseEntity<List<CustomerDto>> getCustomersByTier(
+            @Parameter(description = "Niveau de fidélité", example = "SILVER")
+            @PathVariable CustomerTier tier) {
+
+        List<CustomerDto> customers = customerService.getCustomersByTier(tier);
+        return ResponseEntity.ok(customers);
+    }
+
+
+
+
+
+
+
+
+
+}
